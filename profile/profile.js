365d46191a31f8dedaff7c8492592d87bb9d0f69
Vandit <vxnnns7@gmail.com>
2024-04-03 23:49:00 +0530

    VS || Done Form Validation on Sign in and Sign Up Page

--- profile/profile.js
+++ profile/profile.js
@@ -9,3 +9,62 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
 	container.classList.remove("sign-up-mode");
 });
+function handleSubmit(event) {
+	event.preventDefault();
+	let obj = {
+		username: document.getElementById("username").value,
+		password: document.getElementById("password").value,
+	}
+	if (!obj.username) {
+		isSuccess = false
+		document.getElementById("errorusername").innerText = "* Please enter username";
+	} else {
+		isSuccess = true
+		document.getElementById("errorusername").innerText = "";
+	}
+	if (!obj.password) {
+		isSuccess = false
+		document.getElementById("errorpasssword").innerText = "* Please enter password";
+	}
+	else {
+		isSuccess = true
+		document.getElementById("errorpasssword").innerText = "";
+	}
+	if (isSuccess) {
+		// console.log(obj);
+	}
+}
+function handleSubmit1(event) {
+	event.preventDefault()
+	let obj1 = {
+		username: document.getElementById("username1").value,
+		email: document.getElementById("email").value,
+		password: document.getElementById("password1").value,
+	}
+	if (!obj1.username) {
+		isSuccess = false
+		document.getElementById("errorusername1").innerText = "* Please enter username";
+	} else {
+		isSuccess = true
+		document.getElementById("errorusername1").innerText = "";
+	}
+	if (!obj1.email) {
+		isSuccess = false
+		document.getElementById("erroremail").innerText = "* Please enter email";
+	}
+	else {
+		isSuccess = true
+		document.getElementById("erroremail").innerText = "";
+	}
+	if (!obj1.password) {
+		isSuccess = false
+		document.getElementById("errorpassword1").innerText = "* Please enter password";
+	}
+	else {
+		isSuccess = true
+		document.getElementById("errorpassword1").innerText = "";
+	}
+	if (isSuccess) {
+		// console.log(obj1);
+	}
+}
