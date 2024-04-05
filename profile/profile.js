1988e2d5f8b763ba35cfea28795f85650fd1fdb5
rhramani <rajramani013@gmail.com>
2024-04-05 14:11:49 +0530

    RR || added regular expression inside the form field

--- profile/profile.js
+++ profile/profile.js
@@ -15,23 +15,40 @@ function handleSubmit(event) {
 		username: document.getElementById("username").value,
 		password: document.getElementById("password").value,
 	}
+
+	let userNameRegex = /^[a-z0-9]+$/
+	let passwordRegex = /^[a-zA-Z0-9!@#$%^&*]{6,8}$/
+
+
 	if (!obj.username) {
 		isSuccess = false
 		document.getElementById("errorusername").innerText = "* Please enter username";
+	} else if (!userNameRegex.test(obj.username)) {
+		isSuccess = false
+		document.getElementById('errorusername').innerText = "* Please enter only lowercase character";
 	} else {
 		isSuccess = true
 		document.getElementById("errorusername").innerText = "";
 	}
+
 	if (!obj.password) {
 		isSuccess = false
 		document.getElementById("errorpasssword").innerText = "* Please enter password";
-	}
-	else {
+	} else if (!passwordRegex.test(obj.password)) {
+		isSuccess = false
+		document.getElementById("errorpassword").innerText = "* Please enter valid password";
+	} else if(!obj.password.length > 8){
+		isSuccess = false
+		document.getElementById("errorpassword").innerText = "* Please enter less than 8 character";
+	} else {
 		isSuccess = true
 		document.getElementById("errorpasssword").innerText = "";
 	}
+
 	if (isSuccess) {
-		// console.log(obj);
+		console.log(obj);
+		document.getElementById('sign-in-form').reset();
+
 	}
 }
 function handleSubmit1(event) {
@@ -41,30 +58,50 @@ function handleSubmit1(event) {
 		email: document.getElementById("email").value,
 		password: document.getElementById("password1").value,
 	}
+	let userNameRegex1 = /^[a-z0-9]+$/
+	let passwordRegex1 = /^[a-zA-Z0-9!@#$%^&*]{6,8}$/
+	let emailRegex = /^[a-zA-Z0-9.]+@[a-zA-Z]+[.][a-zA-Z]{2,3}$/
+
 	if (!obj1.username) {
 		isSuccess = false
 		document.getElementById("errorusername1").innerText = "* Please enter username";
-	} else {
+	}
+	else if (!userNameRegex1.test(obj1.username)) {
+		isSuccess = false
+		document.getElementById('errorusername1').innerText = "* Please enter only character(Lowercase) and digit";
+	}
+	else {
 		isSuccess = true
 		document.getElementById("errorusername1").innerText = "";
 	}
+
 	if (!obj1.email) {
 		isSuccess = false
 		document.getElementById("erroremail").innerText = "* Please enter email";
-	}
-	else {
+	} else if (!emailRegex.test(obj1.email)) {
+		isSuccess = false
+		document.getElementById("erroremail").innerText = "* Please enter valid email";
+	} else {
 		isSuccess = true
 		document.getElementById("erroremail").innerText = "";
 	}
+
 	if (!obj1.password) {
 		isSuccess = false
 		document.getElementById("errorpassword1").innerText = "* Please enter password";
-	}
-	else {
+	} else if (!passwordRegex1.test(obj1.password)) {
+		isSuccess = false
+		document.getElementById("errorpassword1").innerText = "* Please enter valid password";
+	} else if(!obj1.password.length > 8){
+		isSuccess = false
+		document.getElementById("errorpassword1").innerText = "* Please enter less than 8 character";
+	}else {
 		isSuccess = true
 		document.getElementById("errorpassword1").innerText = "";
 	}
+
 	if (isSuccess) {
-		// console.log(obj1);
+		console.log(obj1);
+		document.getElementById('sign-up-form').reset();
 	}
 }
