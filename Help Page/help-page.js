a4e7ba9360961198e9ef1920f3728e765811b3df
rhramani <rajramani013@gmail.com>
2024-04-17 09:10:29 +0530

    RR || added and modified help page, and delete account file

--- Help Page/help-page.js
+++ Help Page/help-page.js
@@ -1,40 +1,12 @@
-function handleSubmit(event) {
-    event.preventDefault()
-    let obj = {
-        "name": document.getElementById('name').value,
-        "email": document.getElementById('email').value,
-        "message": document.getElementById('message').value
-    }
-    console.log(obj);
+document.getElementById("review").addEventListener("focus", function () {
+    this.style.backgroundColor = "white";
+});
+document.getElementById("review").addEventListener("blur", function () {
+    this.style.backgroundColor = "#1B263B";
+});
 
-
-    let isSuccess = true
-    if (!obj.name) {
-        isSuccess = false
-        document.getElementById('errorName').innerText = "Please enter name"
-    } else {
-        isSuccess = true
-        document.getElementById('errorName').innerText = ""
-    }
-    if (!obj.email) {
-        isSuccess = false
-        document.getElementById('errorEmail').innerText = "Please enter email"
-    } else {
-        isSuccess = true
-        document.getElementById('errorEmail').innerText = ""
-    }
-    if (!obj.message) {
-        isSuccess = false
-        document.getElementById('errorMessage').innerText = "Please enter message"
-    } else {
-        isSuccess = true
-        document.getElementById('errorMessage').innerText = ""
-    }
-    if (isSuccess) {
-
-        document.getElementById('contactForm').reset()
-
-    }
-
-}
-
+$(document).ready(function () {
+    $('.faq-question').click(function () {
+        $(this).parents('.card').next().find('.collapse').collapse('show');
+    });
+});
